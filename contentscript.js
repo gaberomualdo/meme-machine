966a2bc2e1c1cf69c8591ebd9b451f414bc2cbdc
xtrp <36894883+xtrp@users.noreply.github.com>
2021-04-12 14:41:11 -0400

    Add animation to modal

--- contentscript.js
+++ contentscript.js
@@ -1,6 +1,6 @@
 var topOffset = 0;
 
-chrome.runtime.onMessage.addListener(function(request, sender) {
+chrome.runtime.onMessage.addListener(function (request, sender) {
   var imageURL = request;
   document.head.innerHTML += `
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +9,7 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
   <div id="MemeMachineChromeExtensionModal">
     <style>body { overflow: hidden !important; }</style>
     <div class="content">
-      <button type="button" onclick="this.parentElement.parentElement.outerHTML = '';">
+      <button type="button">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M23 20.168l-8.185-8.187 8.185-8.174-2.832-2.807-8.182 8.179-8.176-8.179-2.81 2.81 8.186 8.196-8.186 8.184 2.81 2.81 8.203-8.192 8.18 8.192z"/></svg>
       </button>
       <canvas></canvas>
@@ -27,24 +27,24 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
   var image = new Image();
   image.src = imageURL;
 
-  var canvas = document.querySelector("div#MemeMachineChromeExtensionModal div.content canvas");
+  var canvas = document.querySelector('div#MemeMachineChromeExtensionModal div.content canvas');
   var context = canvas.getContext('2d');
 
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 
-  function drawMemeImage(){
+  function drawMemeImage() {
     var width = image.width * (canvas.height / image.height);
-    if(width > canvas.height){
+    if (width > canvas.height) {
       var height = image.height * (canvas.width / image.width);
       context.drawImage(image, 0, (canvas.height - height) / 2, canvas.width, height);
       topOffset = (canvas.height - height) / 2;
-    }else{
+    } else {
       context.drawImage(image, (canvas.width - width) / 2, 0, width, canvas.height);
       topOffset = 0;
     }
   }
-  function clearCanvas(){
+  function clearCanvas() {
     context.clearRect(0, 0, canvas.width, canvas.height);
   }
 
@@ -54,24 +54,54 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
     return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
   }
 
-  function updateText(){
+  function updateText() {
     clearCanvas();
     drawMemeImage();
-    context.font = "2.625rem Impact";
-    context.textAlign = "center";
-    context.fillStyle = "#fff";
+    context.font = '2.625rem Impact';
+    context.textAlign = 'center';
+    context.fillStyle = '#fff';
     context.lineWidth = 2;
-    context.fillText(document.querySelector("div#MemeMachineChromeExtensionModal div.content div.row div.inputs input:first-child").value, canvas.width / 2, topOffset + remToPx(3));
-    context.strokeText(document.querySelector("div#MemeMachineChromeExtensionModal div.content div.row div.inputs input:first-child").value, canvas.width / 2, topOffset + remToPx(3));
-    context.fillText(document.querySelector("div#MemeMachineChromeExtensionModal div.content div.row div.inputs input:last-child").value, canvas.width / 2, canvas.height - topOffset - remToPx(1));
-    context.strokeText(document.querySelector("div#MemeMachineChromeExtensionModal div.content div.row div.inputs input:last-child").value, canvas.width / 2, canvas.height - topOffset - remToPx(1));
+    context.fillText(
+      document.querySelector('div#MemeMachineChromeExtensionModal div.content div.row div.inputs input:first-child').value,
+      canvas.width / 2,
+      topOffset + remToPx(3)
+    );
+    context.strokeText(
+      document.querySelector('div#MemeMachineChromeExtensionModal div.content div.row div.inputs input:first-child').value,
+      canvas.width / 2,
+      topOffset + remToPx(3)
+    );
+    context.fillText(
+      document.querySelector('div#MemeMachineChromeExtensionModal div.content div.row div.inputs input:last-child').value,
+      canvas.width / 2,
+      canvas.height - topOffset - remToPx(1)
+    );
+    context.strokeText(
+      document.querySelector('div#MemeMachineChromeExtensionModal div.content div.row div.inputs input:last-child').value,
+      canvas.width / 2,
+      canvas.height - topOffset - remToPx(1)
+    );
   }
 
-  document.querySelector("div#MemeMachineChromeExtensionModal div.content div.row div.inputs input:first-child").oninput = updateText;
-  document.querySelector("div#MemeMachineChromeExtensionModal div.content div.row div.inputs input:last-child").oninput = updateText;
+  document.querySelector('div#MemeMachineChromeExtensionModal div.content div.row div.inputs input:first-child').oninput = updateText;
+  document.querySelector('div#MemeMachineChromeExtensionModal div.content div.row div.inputs input:last-child').oninput = updateText;
 
+  document.querySelector('div#MemeMachineChromeExtensionModal div.content div.row > button').onclick = function () {
+    chrome.runtime.sendMessage({
+      imageURL: imageURL,
+      width: canvas.width,
+      height: canvas.height,
+      topText: document.querySelector('div#MemeMachineChromeExtensionModal div.content div.row div.inputs input:first-child').value,
+      bottomText: document.querySelector('div#MemeMachineChromeExtensionModal div.content div.row div.inputs input:last-child').value,
+    });
+  };
 
-  document.querySelector("div#MemeMachineChromeExtensionModal div.content div.row > button").onclick = function(){
-    chrome.runtime.sendMessage({imageURL: imageURL, width: canvas.width, height: canvas.height, topText: document.querySelector("div#MemeMachineChromeExtensionModal div.content div.row div.inputs input:first-child").value, bottomText: document.querySelector("div#MemeMachineChromeExtensionModal div.content div.row div.inputs input:last-child").value});
-  }
+  const __closeBtn = document.querySelector('div#MemeMachineChromeExtensionModal div.content > button');
+  __closeBtn.addEventListener('click', () => {
+    const __modal = __closeBtn.parentElement.parentElement;
+    __modal.classList.add('closed');
+    setInterval(() => {
+      __modal.outerHTML = '';
+    }, 250);
+  });
 });
--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-document.body.setAttribute("style","filter: blur(10px);");
+document.body.setAttribute('style', 'filter: blur(10px);');
 chrome.contextMenus.create({
-  type: "normal",
-  title: "Make Meme with MemeMachine!",
-  contexts: ["image"],
-  onclick: function(info){
+  type: 'normal',
+  title: 'Make Meme with MemeMachine!',
+  contexts: ['image'],
+  onclick: function (info) {
     var imageURL = info.srcUrl;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, imageURL);
     });
-  }
+  },
 });
-chrome.runtime.onMessage.addListener(function(request, sender) {
-
+chrome.runtime.onMessage.addListener(function (request, sender) {
   var image = new Image();
   image.src = request.imageURL;
 
@@ -20,13 +19,13 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
   canvas.width = request.width;
   canvas.height = request.height;
 
-  function drawMemeImage(){
+  function drawMemeImage() {
     var width = image.width * (canvas.height / image.height);
-    if(width > canvas.height){
+    if (width > canvas.height) {
       var height = image.height * (canvas.width / image.width);
       context.drawImage(image, 0, (canvas.height - height) / 2, canvas.width, height);
       topOffset = (canvas.height - height) / 2;
-    }else{
+    } else {
       context.drawImage(image, (canvas.width - width) / 2, 0, width, canvas.height);
       topOffset = 0;
     }
@@ -35,7 +34,7 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
     return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
   }
   function downloadURI(uri, name) {
-    var link = document.createElement("a");
+    var link = document.createElement('a');
     link.download = name;
     link.href = uri;
     document.body.appendChild(link);
@@ -44,17 +43,17 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
     delete link;
   }
 
-  function loaded(){
+  function loaded() {
     drawMemeImage();
-    context.font = "2.625rem Impact";
-    context.textAlign = "center";
-    context.fillStyle = "#fff";
+    context.font = '2.625rem Impact';
+    context.textAlign = 'center';
+    context.fillStyle = '#fff';
     context.lineWidth = 2;
     context.fillText(request.topText, canvas.width / 2, topOffset + remToPx(3));
     context.strokeText(request.topText, canvas.width / 2, topOffset + remToPx(3));
     context.fillText(request.bottomText, canvas.width / 2, canvas.height - topOffset - remToPx(1));
     context.strokeText(request.bottomText, canvas.width / 2, canvas.height - topOffset - remToPx(1));
-    downloadURI(canvas.toDataURL(), "meme.png");
+    downloadURI(canvas.toDataURL(), 'meme.png');
   }
 
   image.onload = loaded;
